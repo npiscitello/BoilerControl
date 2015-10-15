91a1ab5639373fbeb9590501ab2cc3287b93a43f
Nick P <nb.piscitello@gmail.com>
2015-10-14 21:36:20 -0400

    restructured project for #include commands

---
+++ .ino.cpp
@@ -0,0 +1,15 @@
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2015-10-14 21:35:46
+
+#include "Arduino.h"
+#include "Circulator.h"
+#include "EEPROM.h"
+#include "IO.h"
+#include "MetaManagement.h"
+void setup() ;
+void loop() ;
+
+
+#include "Main.ino"
---
+++ Release/subdir.mk
@@ -0,0 +1,77 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+INO_SRCS += \
+../Main.ino 
+
+CPP_SRCS += \
+../.ino.cpp \
+../Circulator.cpp \
+../EEPROM.cpp \
+../IO.cpp \
+../MetaManagement.cpp 
+
+LINK_OBJ += \
+./.ino.cpp.o \
+./Circulator.cpp.o \
+./EEPROM.cpp.o \
+./IO.cpp.o \
+./MetaManagement.cpp.o 
+
+INO_DEPS += \
+./Main.ino.d 
+
+CPP_DEPS += \
+./.ino.cpp.d \
+./Circulator.cpp.d \
+./EEPROM.cpp.d \
+./IO.cpp.d \
+./MetaManagement.cpp.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+.ino.cpp.o: ../.ino.cpp
+	@echo 'Building file: $<'
+	@echo 'Starting C++ compile'
+	"C:\Arduino\hardware\tools\avr/bin/avr-g++" -c -g -Os -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -MMD -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10605 -DARDUINO_AVR_MINI -DARDUINO_ARCH_AVR     -I"C:\Arduino\hardware\arduino\avr\cores\arduino" -I"C:\Arduino\hardware\arduino\avr\variants\eightanaloginputs" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -D__IN_ECLIPSE__=1 -x c++ "$<"  -o  "$@"   -Wall
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Circulator.cpp.o: ../Circulator.cpp
+	@echo 'Building file: $<'
+	@echo 'Starting C++ compile'
+	"C:\Arduino\hardware\tools\avr/bin/avr-g++" -c -g -Os -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -MMD -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10605 -DARDUINO_AVR_MINI -DARDUINO_ARCH_AVR     -I"C:\Arduino\hardware\arduino\avr\cores\arduino" -I"C:\Arduino\hardware\arduino\avr\variants\eightanaloginputs" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -D__IN_ECLIPSE__=1 -x c++ "$<"  -o  "$@"   -Wall
+	@echo 'Finished building: $<'
+	@echo ' '
+
+EEPROM.cpp.o: ../EEPROM.cpp
+	@echo 'Building file: $<'
+	@echo 'Starting C++ compile'
+	"C:\Arduino\hardware\tools\avr/bin/avr-g++" -c -g -Os -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -MMD -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10605 -DARDUINO_AVR_MINI -DARDUINO_ARCH_AVR     -I"C:\Arduino\hardware\arduino\avr\cores\arduino" -I"C:\Arduino\hardware\arduino\avr\variants\eightanaloginputs" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -D__IN_ECLIPSE__=1 -x c++ "$<"  -o  "$@"   -Wall
+	@echo 'Finished building: $<'
+	@echo ' '
+
+IO.cpp.o: ../IO.cpp
+	@echo 'Building file: $<'
+	@echo 'Starting C++ compile'
+	"C:\Arduino\hardware\tools\avr/bin/avr-g++" -c -g -Os -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -MMD -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10605 -DARDUINO_AVR_MINI -DARDUINO_ARCH_AVR     -I"C:\Arduino\hardware\arduino\avr\cores\arduino" -I"C:\Arduino\hardware\arduino\avr\variants\eightanaloginputs" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -D__IN_ECLIPSE__=1 -x c++ "$<"  -o  "$@"   -Wall
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Main.o: ../Main.ino
+	@echo 'Building file: $<'
+	@echo 'Starting C++ compile'
+	"C:\Arduino\hardware\tools\avr/bin/avr-g++" -c -g -Os -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -MMD -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10605 -DARDUINO_AVR_MINI -DARDUINO_ARCH_AVR     -I"C:\Arduino\hardware\arduino\avr\cores\arduino" -I"C:\Arduino\hardware\arduino\avr\variants\eightanaloginputs" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -D__IN_ECLIPSE__=1 -x c++ "$<"  -o  "$@"   -Wall
+	@echo 'Finished building: $<'
+	@echo ' '
+
+MetaManagement.cpp.o: ../MetaManagement.cpp
+	@echo 'Building file: $<'
+	@echo 'Starting C++ compile'
+	"C:\Arduino\hardware\tools\avr/bin/avr-g++" -c -g -Os -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -MMD -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10605 -DARDUINO_AVR_MINI -DARDUINO_ARCH_AVR     -I"C:\Arduino\hardware\arduino\avr\cores\arduino" -I"C:\Arduino\hardware\arduino\avr\variants\eightanaloginputs" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -D__IN_ECLIPSE__=1 -x c++ "$<"  -o  "$@"   -Wall
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
