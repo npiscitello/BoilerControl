18c30bb30bbb631b39e9fa3e1afef091493d806f
Nick P <nb.piscitello@gmail.com>
2015-10-14 21:07:35 -0400

    first commit - setup project structure

---
+++ Main/Main.ino
@@ -0,0 +1,12 @@
+#include "Circulator.h";
+#include "EEPROM.h";
+#include "IO.h";
+#include "MetaManagement.h"
+
+void setup() {
+
+}
+
+void loop() {
+
+}
---
+++ spec.d
@@ -0,0 +1,2 @@
+spec.o: \
+ C:/Users/piscitellon/workspace/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
